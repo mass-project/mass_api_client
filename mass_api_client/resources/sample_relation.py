36f8cc0fa804435aac15c5b9c770176f67d9d705
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-07-23 21:00:24 +0200

    Added convenience functions and fixed missing creation points

--- mass_api_client/resources/sample_relation.py
+++ mass_api_client/resources/sample_relation.py
@@ -1,3 +1,5 @@
+from requests.exceptions import HTTPError
+
 from mass_api_client.schemas import SampleRelationTypeSchema, SampleRelationSchema
 from .base_with_subclasses import BaseResource
 from .sample import Sample
@@ -6,10 +8,37 @@ from .sample import Sample
 class SampleRelationType(BaseResource):
     schema = SampleRelationTypeSchema()
     endpoint = 'sample_relation_type'
+    creation_point = endpoint
+
+    filter_parameters = ['name']
 
     @classmethod
     def create(cls, name, directed, **kwargs):
         return cls._create(name=name, directed=directed)
+
+    @classmethod
+    def get_or_create(cls, name, directed, **kwargs):
+        """
+        Try to fetch the `SampleRelationType` from the server and create it if it does not exist.
+
+        :param name: The unique name of the `SampleRelationType`
+        :param directed: bool
+        :param kwargs:
+        :return: The `SampleRelationType`
+        :raises `ValueError` if the `SampleRelationType` already exists on the server, but the value of 'directed' does not match.
+        """
+        try:
+            obj = cls.get(name)
+        except HTTPError:
+            obj = cls.create(name, directed, **kwargs)
+
+        if obj.directed != directed:
+            raise ValueError('The SampleRelationType exists on the server, but the value of "directed" does not match.')
+
+        return obj
+
+    def create_relation(self, sample, other, **kwargs):
+        return SampleRelation.create(sample, other, self, **kwargs)
 
     def __repr__(self):
         return '[{}] {}'.format(str(self.__class__.__name__), str(self.name))
@@ -21,6 +50,7 @@ class SampleRelationType(BaseResource):
 class SampleRelation(BaseResource):
     schema = SampleRelationSchema()
     endpoint = 'sample_relation'
+    creation_point = endpoint
     _class_identifier = 'SampleRelation'
 
     @classmethod
