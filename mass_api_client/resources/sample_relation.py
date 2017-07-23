eea226f990e7e63121b694e269213ad247d78fd8
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-07-24 01:26:12 +0200

    Removed kwargs from create methods, to stay consistent with other resources

--- mass_api_client/resources/sample_relation.py
+++ mass_api_client/resources/sample_relation.py
@@ -13,32 +13,31 @@ class SampleRelationType(BaseResource):
     filter_parameters = ['name']
 
     @classmethod
-    def create(cls, name, directed, **kwargs):
+    def create(cls, name, directed):
         return cls._create(name=name, directed=directed)
 
     @classmethod
-    def get_or_create(cls, name, directed, **kwargs):
+    def get_or_create(cls, name, directed):
         """
         Try to fetch the `SampleRelationType` from the server and create it if it does not exist.
 
         :param name: The unique name of the `SampleRelationType`
         :param directed: bool
-        :param kwargs:
         :return: The `SampleRelationType`
         :raises `ValueError` if the `SampleRelationType` already exists on the server, but the value of 'directed' does not match.
         """
         try:
             obj = cls.get(name)
         except HTTPError:
-            obj = cls.create(name, directed, **kwargs)
+            obj = cls.create(name, directed)
 
         if obj.directed != directed:
             raise ValueError('The SampleRelationType exists on the server, but the value of "directed" does not match.')
 
         return obj
 
-    def create_relation(self, sample, other, **kwargs):
-        return SampleRelation.create(sample, other, self, **kwargs)
+    def create_relation(self, sample, other, additional_metadata=None):
+        return SampleRelation.create(sample, other, self, additional_metadata)
 
     def __repr__(self):
         return '[{}] {}'.format(str(self.__class__.__name__), str(self.name))
@@ -54,11 +53,14 @@ class SampleRelation(BaseResource):
     _class_identifier = 'SampleRelation'
 
     @classmethod
-    def create(cls, sample, other, relation_type, **kwargs):
+    def create(cls, sample, other, relation_type, additional_metadata=None):
         if not isinstance(sample, Sample) or not isinstance(other, Sample):
             raise ValueError('"sample" and "other" must be an instance of Sample')
 
-        return cls._create(sample=sample.url, other=other.url, relation_type=relation_type.url, **kwargs)
+        if additional_metadata is None:
+            additional_metadata = {}
+
+        return cls._create(sample=sample.url, other=other.url, relation_type=relation_type.url, additional_metadata=additional_metadata)
 
     def __repr__(self):
         return '[{}] {}'.format(str(self.__class__.__name__), str(self.id))
