c0c2507ab1174d6042b55f1baf29b5564c56f409
requires.io <support@requires.io>
2018-06-12 20:37:25 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(name='mass_api_client',
       version=version,
       license='MIT',
       url='https://github.com/mass-project/mass_api_client',
-      install_requires=['requests==2.18.4', 'marshmallow==2.15.3'],
+      install_requires=['requests==2.19.0', 'marshmallow==2.15.3'],
       packages=find_packages(),
       )
